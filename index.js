0e0d54e6cf632b2f64eb4b2fd7b0798c75dc0e20
mung <dai.suan.mung@powercoders.org>
2021-12-23 00:33:27 +0100

    adding actions title

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-app.get("/",(req,res)=> res.send("Welcome from Azure Node Docker"))
+app.get("/",(req,res)=> res.send("Welcome from Azure Node Docker with Actions"))
 
 app.listen(3000, ()=>{
     console.log("my port run in 3000")
